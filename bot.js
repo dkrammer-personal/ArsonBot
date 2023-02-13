3884f01a13b544f573f7ac840db5e773f6fcd829
Duncan Krammer <duncankrammer@gmail.com>
2023-02-13 15:43:10 -0700

    Bot notifies @everyone every Sunday, added bash script

--- bot.js
+++ bot.js
@@ -13,76 +13,78 @@ let guild, voiceChannel, textChannel ;
 Client.on('ready', async () => {
 	try {
 		guild = await Client.guilds.fetch(GUILD_ID);
-		voiceChannel = guild.channels.cache.get(VOICE_CHANNEL_ID);
+		// voiceChannel = guild.channels.cache.get(VOICE_CHANNEL_ID);
 	} catch (error) {
 		console.log(error);
 		process.exit(1);
 	}
 	textChannel = guild.channels.cache.get(TEXT_CHANNEL_ID);
-	console.log('Big Ben Ready...');
-	Client.user.setPresence({ activity: { name: 'the hour', type: 'WATCHING' }, status: 'idle' });
+	console.log('ArsonBot is ready to burn...');
+	Client.user.setPresence({ activity: { name: 'it burn...', type: 'WATCHING' }});
 });
 
-// use node-cron to create a job to run every hour
-const task = cron.schedule('0 0 */1 * * *', async () => {
-	let { hour, amPm, timezoneOffsetString } = getTimeInfo();
+// use node-cron to create a job to run every Sunday at 9am
+// cron.schedule format goes '* * * * * *', as follows:
+// 'sec min hour(0-23) dayOfMonth(1-31) month(1-12 || names) dayOfWeek(0-7 || names, 0 and 7 are Sunday)
+const task = cron.schedule('0 0 13 * * 0', async () => {
+	// let { hour, amPm, timezoneOffsetString } = getTimeInfo();
 
 	// if text channel was defined send message in chat
 	if (textChannel) {
-		const messageEmbed = new Discord.MessageEmbed()
-		.setColor('#FFD700')
-		.setTitle(`The time is now ${hour}:00 ${amPm} GMT${timezoneOffsetString}`)
+		// const messageEmbed = new Discord.MessageEmbed()
+		// .setColor('#FFD700')
+		// .setTitle(`The time is now ${hour}:00 ${amPm} GMT${timezoneOffsetString}`)
 		
-		textChannel.send(messageEmbed);
+		textChannel.send("@everyone, don't forget to update your dev logs today!");
 	}
 
-	// check if VC defined in config is empty
-	if (voiceChannel.members.size >= 1) {
-		try {
-			Client.user.setPresence({ activity: { name: 'the big bell', type: 'PLAYING' }, status: 'available' });
-			// connect to voice channel
-			const connection = await voiceChannel.join();
-			// counter for looping
-			let count = 1;
+	// // check if VC defined in config is empty
+	// if (voiceChannel.members.size >= 1) {
+	// 	try {
+	// 		Client.user.setPresence({ activity: { name: 'the big bell', type: 'PLAYING' }, status: 'available' });
+	// 		// connect to voice channel
+	// 		const connection = await voiceChannel.join();
+	// 		// counter for looping
+	// 		let count = 1;
 		
-			// immediately invoked function that loops to play the bell sound 
-			(function play(Client) {
-				connection.play('bigben.mp3')
-				.on('finish', () => {
-					count += 1;
-					if (count <= hour && MATCH_DINGS_WITH_HOUR == 'true') {
-						play(Client);
-					} else {
-						connection.disconnect();
-						Client.user.setPresence({ activity: { name: 'the hour', type: 'WATCHING' }, status: 'idle' });
-					}
-				})
-			})();
+	// 		// immediately invoked function that loops to play the bell sound 
+	// 		(function play(Client) {
+	// 			connection.play('bigben.mp3')
+	// 			.on('finish', () => {
+	// 				count += 1;
+	// 				if (count <= hour && MATCH_DINGS_WITH_HOUR == 'true') {
+	// 					play(Client);
+	// 				} else {
+	// 					connection.disconnect();
+	// 					Client.user.setPresence({ activity: { name: 'the hour', type: 'WATCHING' }, status: 'idle' });
+	// 				}
+	// 			})
+	// 		})();
 
-		} catch(error) {
-			console.log(error);
-		}
-	}
+	// 	} catch(error) {
+	// 		console.log(error);
+	// 	}
+	// }
 });
 
 // function to get current time and return object containing
 // hour and if it is am or pm
-const getTimeInfo = () => {
-		let time = new Date();
-		let hour = time.getHours() >= 12 ? time.getHours() - 12 : time.getHours();
-		hour = hour === 0 ? 12 : hour;
-		let amPm = time.getHours() >= 12 ? 'PM' : 'AM';
-		// get gmt offset in minutes and convert to hours
-		let gmtOffset = time.getTimezoneOffset() / 60
-		// turn gmt offset into a string representing the timezone in its + or - gmt offset
-		let timezoneOffsetString = `${gmtOffset > 0 ? '-':'+'} ${Math.abs(gmtOffset)}`;
+// const getTimeInfo = () => {
+// 		let time = new Date();
+// 		let hour = time.getHours() >= 12 ? time.getHours() - 12 : time.getHours();
+// 		hour = hour === 0 ? 12 : hour;
+// 		let amPm = time.getHours() >= 12 ? 'PM' : 'AM';
+// 		// get gmt offset in minutes and convert to hours
+// 		let gmtOffset = time.getTimezoneOffset() / 60
+// 		// turn gmt offset into a string representing the timezone in its + or - gmt offset
+// 		let timezoneOffsetString = `${gmtOffset > 0 ? '-':'+'} ${Math.abs(gmtOffset)}`;
 
-	return {
-		hour,
-		amPm,
-		timezoneOffsetString
-	}
-}
+// 	return {
+// 		hour,
+// 		amPm,
+// 		timezoneOffsetString
+// 	}
+// }
 
 // start the cron job
 task.start();
