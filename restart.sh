f2d90cacae8cdb2bbc05cc12c2eff69819dc823b
Duncan Krammer <duncankrammer@gmail.com>
2023-02-26 23:31:15 -0700

    Clean project to redo dependencies

--- restart.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-kill -9 
-node bot.js
--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-node bot.js
-
-# run "ps aux | grep bot.js | grep root" to find PID of process
-# run "sudo kill -9 <PID>" to kill process
-# run "sudo setsid ./run.sh &" to restart process
